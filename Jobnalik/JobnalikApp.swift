2d2e1071c6fb51af8caaff6d1e944203b3a025d8
kevinumarov <keulifeinfluencer@gmail.com>
2024-11-11 19:42:12 +0900

    Initial Commit

---
+++ Jobnalik/JobnalikApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobnalikApp.swift
+//  Jobnalik
+//
+//  Created by Kevin Umarov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct JobnalikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobnalikTests/JobnalikTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobnalikTests.swift
+//  JobnalikTests
+//
+//  Created by Kevin Umarov on 11/11/24.
+//
+
+import Testing
+@testable import Jobnalik
+
+struct JobnalikTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
